dc6872c18026a94197fb416fba225e1eb1f8b71d
unknown <olejarz-piotr@o2.pl>
2021-12-10 00:27:28 +0100

    creating styles for Chat and Join, styles from github project owner

--- server/index.js
+++ server/index.js
@@ -2,11 +2,20 @@ const express = require('express');
 const socketio = require('socket.io');
 const router = require('./router.js')
 const http = require('http');
+
 const PORT = process.env.PORT || 5000;
 const app = express();
 const server = http.createServer(app);
 const io = socketio(server);
 
+io.on('connection', (socket) => {
+    console.log('new connection');
+
+    socket.on('disconnect', () => {
+        console.log('user has left');
+        
+    })
+})
 app.use(router);
 
 server.listen(PORT,()=>{console.log(`server running on port ${PORT}`);})
