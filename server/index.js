c275e60c03962a083ce9591890a4cfc1e2cc230b
unknown <olejarz-piotr@o2.pl>
2021-12-09 23:51:50 +0100

    creating router node.js

--- server/index.js
+++ server/index.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const socketio = require('socket.io');
+const router = require('./router.js')
 const http = require('http');
 const PORT = process.env.PORT || 5000;
 const app = express();
 const server = http.createServer(app);
 const io = socketio(server);
+
+app.use(router);
 
 server.listen(PORT,()=>{console.log(`server running on port ${PORT}`);})
