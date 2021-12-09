d4a3c775e830e6f15c890932e051ad25382ae665
unknown <olejarz-piotr@o2.pl>
2021-12-09 02:29:31 +0100

    creating Routes in App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,21 @@
-import React from 'react';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import React from "react";
+import {
+  BrowserRouter,
+  BrowserRouter as Routes,
+  Route,
+} from "react-router-dom";
+import Join from "./components/Join";
+import Chat from "./components/Chat";
 
-const App = () => { 
-    <Router>
-        <Route path="/" exact component={Join} />
-        <Route path ="/chat" exact component ={Chat}/>
-
-    </Router>
+const App = () => {
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" exact component={Join} />
+                <Route path="/chat" exact component={Chat} />
+            </Routes>
+        </BrowserRouter>
+    );
 }
+
 export default App;
