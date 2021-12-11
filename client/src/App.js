e313459ed2da6898566037cca0c8eeb8a8be3108
unknown <olejarz-piotr@o2.pl>
2021-12-11 12:48:04 +0100

    configuration front end with backend to connect via socket

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,11 @@
-import React from "react";
+import React from "react"; 
 import {
   BrowserRouter,
   BrowserRouter as Routes,
   Route,
 } from "react-router-dom";
-import Join from "./components/Join";
-import Chat from "./components/Chat";
+import Join from "./components/Join/Join";
+import Chat from "./components/Chat/Chat";
 
 const App = () => {
     return (
--- server/index.js
+++ server/index.js
@@ -11,6 +11,13 @@ const io = socketio(server);
 io.on('connection', (socket) => {
     console.log('new connection');
 
+    socket.on('join', ({name,room},callback) => {
+        console.log(name, room);
+
+    
+
+    })
+
     socket.on('disconnect', () => {
         console.log('user has left');
         
